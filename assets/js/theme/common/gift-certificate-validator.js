4e612e720a618f3e69c6d6d17ee5785f14a58c28
Sviataslau <svushev@webycorp.com>
2023-03-13 15:21:57 +0100

    removed old template

--- assets/js/theme/common/gift-certificate-validator.js
+++
@@ -1,8 +0,0 @@
-export default function (cert) {
-    if (typeof cert !== 'string') {
-        return false;
-    }
-
-    // Add any custom gift certificate validation logic here
-    return true;
-}
